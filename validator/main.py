52d7f0ffd6ad0c7438aff62184ac6a3e42babf83
Joseph Catrambone <joseph@guardrailsai.com>
2024-12-06 09:09:53 -0800

    If validator is passed a single sentence then it should return a single sentence, not a list.

--- validator/main.py
+++ validator/main.py
@@ -1,6 +1,7 @@
 import os
+import re
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Tuple, Optional
+from typing import Callable, Dict, List, Optional, Union
 
 import spacy
 from cached_path import cached_path
@@ -12,7 +13,7 @@ from guardrails.validator_base import (
     register_validator,
 )
 from guardrails.types import OnFailAction
-from transformers import pipeline, TFAutoModel, AutoTokenizer
+from transformers import pipeline
 
 
 S3_SPACY_NLP_MODEL_PATH = "s3://guardrails-ai-public-read-only/bias_check/dbias_0_1_5_en_pipeline.tar.gz"
@@ -70,7 +71,6 @@ class BiasCheck(Validator):
 
     @staticmethod
     def prefetch_models():
-        print("Downloading bias classification model:")
         # Despite passing `from_tf=True,` into the pipeline, some versions of
         # transformers will complain about loading from TF models. Using this wonky
         # combination of TFAutoModel and tokenizer, we can get it to load.
@@ -79,18 +79,22 @@ class BiasCheck(Validator):
             model="d4data/bias-detection-model",
             tokenizer="d4data/bias-detection-model",
         )
-        print("Downloading bias words detector:")
         bias_words_detector = spacy.load(cached_path(
             f"{S3_SPACY_NLP_MODEL_PATH}!dbias_0_1_5_en_pipeline",
             cache_dir=MODEL_CACHE_DIR, extract_archive=True
         ))
-        print("Downloading masked word model:")
         masked_word_model = pipeline('fill-mask', model='bert-base-cased')
         return classification_pipe, bias_words_detector, masked_word_model
 
-    def validate(self, value: List[str], metadata: Optional[Dict] = None) -> ValidationResult:
+    def validate(
+            self,
+            value: Union[str, List[str]],
+            metadata: Optional[Dict] = None
+    ) -> ValidationResult:
         """Validates that the text is free from biases related to age, gender, sex, ethnicity, religion, etc."""
+        single_sentence_passed = False
         if isinstance(value, str):
+            single_sentence_passed = True
             value = [value,]  # Ensure we're always passing lists of strings into the classifier.
 
         scores = self._inference(value)
@@ -116,18 +120,18 @@ class BiasCheck(Validator):
             # Four paths: noop, exception, fix, filter.
             # self.on_fail_method == NOOP or FILTER, return only passing outputs.
             # EXCEPTION is handled farther up the stack, which leaves us only 'fix'.
-            if self.on_fail_method != OnFailAction.FIX:
-                fix_value = " ".join(passing_outputs)
+            if self.on_fail_descriptor != OnFailAction.FIX:
+                fix_value = passing_outputs
             else:
-                fix_value = ""
+                fix_value = list()
                 for needs_fix, text in all_outputs:
                     if not needs_fix:
-                        fix_value += text + " "
+                        fix_value.append(text)
                     else:
-                        pass
+                        fix_value.append(self.fix_sentence(text))
             return FailResult(
                 error_message=failure_message,
-                fix_value=fix_value,
+                fix_value=" ".join(fix_value) if single_sentence_passed else fix_value,
             )
         return PassResult()
 
@@ -147,20 +151,25 @@ class BiasCheck(Validator):
         return scores
 
     def fix_sentence(self, sentence: str) -> str:
-        # The original DBias implementation would brute-force all combinations of masks.
-        # We do a greedy search instead, picking the minimally charged option for each.
-        # Like the original, there's no guarantee of maintaining the pragmatics of the
-        # starting sentence, but to accomplish that we would need to train a new model.
+        """The original DBias algorithm would brute-force, potentially O(2^n) operation.
+        This performs a similar evaluation, but greedily replaces words instead of
+        trying all combinations. Since the original did not preserve semantics or
+        pragmatics, these will approach something not dissimilar, but they have
+        different theoretical guarantees about proximity to the original."""
         start_sentence = sentence
         starting_bias = self._inference_local([sentence,])[0]
         if starting_bias < self.threshold:
-            pass  # Should we raise an exception here?  Starting under threshold?
+            # Should we raise an exception here?  Starting under threshold?
+            return start_sentence
         charged_words = [t.text for t in self.bias_words_detector(sentence).ents]
         for word_to_replace in charged_words:
             for _ in range(0, start_sentence.count(word_to_replace)):
                 temp = start_sentence.replace(word_to_replace, "[MASK]", 1)
                 # Generate a bunch of candidate sentences:
-                candidate_sentences = [x['sequence'] for x in self.unmasker(temp)]
+                candidate_sentences = list()
+                for x in self.unmasker(temp):
+                    if x['token'] not in charged_words:
+                        candidate_sentences.append(x['sequence'])
                 # Score them and take the best:
                 scores = self._inference_local(candidate_sentences)
                 best_score, best_text = argmin_pair(scores, candidate_sentences)
